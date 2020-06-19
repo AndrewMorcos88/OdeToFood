6e4b7ec6190f41d139974c49278fadfd90fbfc8c
--replace-all <andromorcos88@gmail.com>
2020-06-19 18:12:18 +0200

    initial commit to test

---
+++ ClassLibrary1/Services/InMemoryRestuarantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestuarantData : IResturantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestuarantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {ID=1,Name="AndrewPizza",Cuisine=CuisineTybe.Egyption},
+                new Restaurant {ID=2,Name="BavlyPizza",Cuisine=CuisineTybe.Indian},
+                new Restaurant {ID=3,Name="Macdonalds",Cuisine=CuisineTybe.Egyption},
+                new Restaurant {ID=1,Name="AndrewPizza",Cuisine=CuisineTybe.American}
+            }; 
+
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r=>r.Name);
+        }
+    }
+}
